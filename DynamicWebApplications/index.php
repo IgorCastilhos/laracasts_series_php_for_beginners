3a31e2f2055925c74226c18fa9c2f4452bb2462b
igorc <igorcastilhos2010@hotmail.com>
2024-08-10 22:27:12 -0300

    add: [x] 5/9 and 6/9 Dynamic Web Applications chapter

--- DynamicWebApplications/index.php
+++ DynamicWebApplications/index.php
@@ -1,4 +1,19 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+// connect to MySQL database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;";
+$username = "root";
+$password = "My9953";
+$pdo = new PDO($dsn, $username, $password);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
